c2b1d36c88f4050f692afe8b78a1596da45d556e
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-06 02:49:22 +0400

    Exercise Completed.

---
+++ variableExercise/index.js
@@ -0,0 +1,7 @@
+// Create two variables, firstName and lastName
+let firstName = "Khadiga";
+let secondName = "Abdullah";
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + secondName;
+// Log fullName to the console
+console.log(fullName);
