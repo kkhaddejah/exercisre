43a6712c5cce217a0293a63ba26e199173adc796
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-13 17:43:19 +0400

    Exercise Completed.

---
+++ concatenate/index.js
@@ -0,0 +1,10 @@
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function concatenate() {
+  console.log(`${greeting}, ${name}!`);
+}
+
+concatenate();
